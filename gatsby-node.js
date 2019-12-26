3717831acec498a9f83d10077d9ae28420e4f3a2
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2019-12-26 13:56:21 -0500

    Idk what's going on but the dependencies are broken and I didn't change anything

--- gatsby-node.js
+++ gatsby-node.js
@@ -150,3 +150,84 @@ exports.createPages = ({ graphql, actions }) => {
         });
     });
 };
+
+// exports.createResolvers = ({
+//     actions,
+//     cache,
+//     createNodeId,
+//     createResolvers,
+//     getNode,
+//     store,
+//     reporter
+//   }) => {
+//     const { createNode, touchNode } = actions;
+  
+//     // Add all media libary images from wordpress so they can be queried by
+//     // childImageSharp to take advantage of Gatsby image processing 
+//     createResolvers({
+//       WPGraphQL_MediaItem: {
+//         imageFile: {
+//           type: `File`,
+//           async resolve(source, args, context, info) {
+//             if (source.sourceUrl) {
+//               let fileNodeID;
+//               let fileNode;
+//               let sourceModified;
+  
+//               // Set the file cacheID, get it (if it has already been set)
+//               const mediaDataCacheKey = `wordpress-media-${source.mediaItemId}`;
+//               const cacheMediaData = await cache.get(mediaDataCacheKey);
+  
+//               if (source.modified) {
+//                 sourceModified = source.modified;
+//               }
+  
+//               // If we have cached media data and it wasn't modified, reuse
+//               // previously created file node to not try to redownload
+//               if (cacheMediaData && sourceModified === cacheMediaData.modified) {
+//                 fileNode = getNode(cacheMediaData.fileNodeID);
+  
+//                 // check if node still exists in cache
+//                 // it could be removed if image was made private
+//                 if (fileNode) {
+//                   fileNodeID = cacheMediaData.fileNodeID;
+//                   touchNode({
+//                     nodeId: fileNodeID
+//                   });
+//                 }
+//               }
+  
+//               // If we don't have cached data, download the file
+//               if (!fileNodeID) {
+//                 try {
+//                   // Get the filenode
+//                   fileNode = await createRemoteFileNode({
+//                     url: source.sourceUrl,
+//                     store,
+//                     cache,
+//                     createNode,
+//                     createNodeId,
+//                     reporter
+//                   });
+  
+//                   if (fileNode) {
+//                     fileNodeID = fileNode.id;
+  
+//                     await cache.set(mediaDataCacheKey, {
+//                       fileNodeID,
+//                       modified: sourceModified
+//                     });
+//                   }
+//                 } catch (e) {
+//                   // Ignore
+//                   console.log(e);
+//                   return null;
+//                 }
+//               }
+//               if (fileNode) return fileNode;
+//             }
+//             return null;
+//           }
+//         }
+//       }
+//     });}
